0835018be73e160b15c6858249ef4667758605c2
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-12-05 00:00:57 +0300

    Create m221204_085911_create_settings_table.php

---
+++ migarations/m221204_085911_create_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `settings`.
+ * 
+ */
+class m221204_085911_create_settings_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'settings',
+            [
+                'id' => $this->primaryKey(),
+                'title' => $this->string()->notNull(),
+                'description' => $this->string(),
+                'type' => $this->string()->notNull(),
+                'setting_group_id' => $this->integer()->notNull(),
+                'key' => $this->string()->notNull()->unique(),
+                'value' => $this->text(),
+            ]
+        );
+        
+        // creates index for column `key`
+        $this->createIndex(
+            'idx-settings-key',
+            'settings',
+            'key'
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('settings');
+        return true;
+    }
+}
