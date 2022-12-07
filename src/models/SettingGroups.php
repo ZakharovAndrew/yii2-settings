b5358c545cb735b5d91f74cfe3708fc2bd161b7f
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-12-08 01:33:07 +0300

    Create SettingGroups.php

---
+++ src/models/SettingGroups.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "setting_groups".
+ *
+ * @property int $id
+ * @property string $title 
+ * @property string $key 
+ * @property int $pos
+ */
+class SettingGroups extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'setting_groups';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'key'], 'required'],
+            [['pos',], 'integer'],
+            [['title', 'key'], 'string', 'max' => 255],
+            [['key'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'key' => 'Key',
+            'pos' => 'Position'
+        ];
+    }
+}
