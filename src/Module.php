8d353a498552bbce40e1d4076d5605146a89f597
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-12-15 14:42:06 +0300

    Add files via upload

--- src/Module.php
+++ src/Module.php
@@ -1,11 +1,17 @@
 <?php
 
 /**
+ * Yii2 Settings
+ * *************
+ * Yii2 settings with database module with GUI manager supported.
+ *  
  * @link https://github.com/ZakharovAndrew/yii2-settings/
  * @copyright Copyright (c) 2022 Zakharov Andrew
  */
  
 namespace ZakharovAndrew\settings;
+
+use Yii;
 
 /**
  * Class Module 
@@ -22,6 +28,12 @@ class Module extends \yii\base\Module
     public $bootstrapVersion = '';
     
     /**
+     *
+     * @var string source language for translation 
+     */
+    public $sourceLanguage = 'en-US';
+    
+    /**
      * @inheritdoc
      */
     public $controllerNamespace = 'ZakharovAndrew\settings\controllers';
@@ -33,6 +45,39 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
+        $this->registerTranslations();
+    }
+    
+    /**
+     * Registers the translation files
+     */
+    protected function registerTranslations()
+    {
+        Yii::$app->i18n->translations['extension/yii2-settings/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => $this->sourceLanguage,
+            'basePath' => '@vendor/zakharov-andrew/yii2-settings/src/messages',
+            'on missingTranslation' => ['app\components\TranslationEventHandler', 'handleMissingTranslation'],
+            'fileMap' => [
+                'extension/yii2-settings/settings' => 'settings.php',
+            ],
+        ];
+    }
+
+    /**
+     * Translates a message. This is just a wrapper of Yii::t
+     *
+     * @see Yii::t
+     *
+     * @param $category
+     * @param $message
+     * @param array $params
+     * @param null $language
+     * @return string
+     */
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('extension/yii2-settings/' . $category, $message, $params, $language);
     }
     
 }
